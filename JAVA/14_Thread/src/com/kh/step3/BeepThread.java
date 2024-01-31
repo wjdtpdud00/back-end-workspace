ea30ee52754bd09cbb37a665f06bb2eaa471a6ba
wjdtpdud00 <152463268+wjdtpdud00@users.noreply.github.com>
2024-01-31 17:28:10 +0900

    inputthread

---
+++ JAVA/14_Thread/src/com/kh/step3/BeepThread.java
@@ -0,0 +1,14 @@
+package com.kh.step3;
+public class BeepThread extends Thread {
+	public void run() {
+		// 띵띵띵띵띵을 5번 출력하는 작업
+		for (int i = 0; i < 5; i++) {
+			System.out.println("띵~~");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}

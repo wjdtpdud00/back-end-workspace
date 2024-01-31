962c09056f56d12ffac0f4a06820b52b1947d12d
wjdtpdud00 <152463268+wjdtpdud00@users.noreply.github.com>
2024-01-31 17:27:58 +0900

    application

---
+++ JAVA/14_Thread/src/com/kh/step2/Application.java
@@ -0,0 +1,18 @@
+package com.kh.step2;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 1. Thread 생성
+		GoThread go = new GoThread();
+		ComeThread come = new ComeThread();
+		
+		Thread tgo = new Thread(go, "GoThread");
+		Thread tcome = new Thread(come, "ComeThread");
+		
+		// 2. start 호출
+		tgo.start();
+		tcome.start();
+	}
+
+}
